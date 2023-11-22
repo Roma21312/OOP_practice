edde1634f5d510995185e362aefdd45d565c6640
Danik <daneilmoroz@gmail.com>
2023-11-22 11:59:51 +0200

    done practical work 4

--- pr1/pr1.cpp
+++ pr1/pr1.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <Windows.h>
 #include "PassengerTrain.h"
 #include "PassengerTransport.h"
 #include "Plain.h"
@@ -7,8 +8,49 @@ using namespace std;
 
 int main()
 {
-	PassengerTrain a;
-	PassengerTransport b;
-	Plain c;
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    PassengerTransport* passengerTransports[5];
+
+        for (int i = 0; i < 5; i++) {
+            int choice = menu();
+
+            switch (choice) {
+            case 1: {
+                PassengerTrain* train = new PassengerTrain();
+                cin >> *train;
+                passengerTransports[i] = train;
+                break;
+            }
+            case 2: {
+                Plain* plain = new Plain();
+                cin >> *plain;
+                passengerTransports[i] = plain;
+                break;
+            }
+            default:
+                {
+                break;
+                }
+            }
+
+            passengerTransports[i]->display();
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            delete passengerTransports[i];
+        }
+
+        return 0;
 }
 
+int menu() 
+{
+    int choice;
+    cout << "Виберіть тип транспорту, який ви хочете створити:\n";
+    cout << "1. Пасажирський поїзд\n";
+    cout << "2. Літак\n";
+    cin >> choice;
+    return choice;
+}
